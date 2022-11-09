088e8bf986a3fb13e66e410f93f4f9605543b9a2
manoflogan <manoflogan@gmail.com>
2022-11-09 07:53:26 -0800

    [Step 2] Getting the repository test to pass after integrating robolectric.

--- app/src/test/java/com/krishnanand/clickrow/repository/ClickRowRepositoryTest.kt
+++ app/src/test/java/com/krishnanand/clickrow/repository/ClickRowRepositoryTest.kt
@@ -5,13 +5,17 @@ import androidx.test.core.app.ApplicationProvider
 import com.squareup.moshi.Moshi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @OptIn(ExperimentalCoroutinesApi::class)
+@Config(sdk = [Config.OLDEST_SDK])
 class ClickRowRepositoryTest {
 
     private lateinit var clickRowRepository: ClickRowRepositoryImpl
@@ -31,6 +35,8 @@ class ClickRowRepositoryTest {
 
     @Test
     fun `validate that the assets are loaded correctly`() = runTest {
-        clickRowRepository.fetchClickRows()
+        val clickRows = clickRowRepository.fetchClickRows()
+        MatcherAssert.assertThat(clickRows, CoreMatchers.notNullValue())
+        MatcherAssert.assertThat(clickRows.size, CoreMatchers.`is`(2))
     }
 }
