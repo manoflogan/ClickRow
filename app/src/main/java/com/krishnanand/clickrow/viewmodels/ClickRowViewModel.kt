4b711335652cc0841fde42587447d77433bee8b4
manoflogan <manoflogan@gmail.com>
2023-01-15 16:58:52 -0800

    [Step 6] Making the view model tests pass. (#5)

--- app/src/main/java/com/krishnanand/clickrow/viewmodels/ClickRowViewModel.kt
+++ app/src/main/java/com/krishnanand/clickrow/viewmodels/ClickRowViewModel.kt
@@ -4,9 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.krishnanand.clickrow.data.ClickRow
 import com.krishnanand.clickrow.repository.ClickRowRepository
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,13 +13,13 @@ class ClickRowViewModel @Inject constructor(
 ): ViewModel() {
 
     private val _clickRowsFlow = MutableStateFlow<List<ClickRow>>(mutableListOf())
-    val clickRowsFlow = _clickRowsFlow.asStateFlow()
+    val clickRowsFlow:Flow<List<ClickRow>> = _clickRowsFlow.filter {
+        it.isNotEmpty()
+    }
 
     fun fetchAndInitialiseClickRows() {
         viewModelScope.launch {
-            _clickRowsFlow.update {
-                clickRowRepository.fetchClickRows()
-            }
+            _clickRowsFlow.value = clickRowRepository.fetchClickRows()
         }
     }
 }
